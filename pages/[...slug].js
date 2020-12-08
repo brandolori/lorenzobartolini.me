45317cbabc5343805fdcdf2faf62102c10746a8d
lorib <lori.barto@live.it>
2020-12-09 00:06:03 +0100

    working path

---
+++ pages/[...slug].js
@@ -0,0 +1,68 @@
+import React from "react"
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
+import Head from "next/head"
+import marked from "marked"
+import { Layout } from "../components/shared"
+import { Jumbotron, Container } from "react-bootstrap"
+
+const folderName = "pages-md"
+
+const Post = ({ htmlString, data }) => (
+    <Layout>
+        <Head>
+            <title>{data.title}</title>
+        </Head>
+        <Container>
+            <main dangerouslySetInnerHTML={{ __html: htmlString }} />
+        </Container>
+
+    </Layout>
+)
+
+
+export const getStaticProps = async ({ params: { slug } }) => {
+
+    let filePath = folderName
+    slug.forEach(el => filePath = path.join(filePath, el))
+    filePath = filePath + ".md"
+    const markDownWithMetadata = fs.readFileSync(filePath).toString()
+    const parsedMarkdown = matter(markDownWithMetadata)
+    const htmlString = marked(parsedMarkdown.content)
+    return {
+        props: {
+            htmlString,
+            data: parsedMarkdown.data
+        }
+    }
+}
+
+
+export const getStaticPaths = async () => {
+
+    const files = []
+    const recurse = (folderName) => {
+        fs.readdirSync(folderName).map(file => path.join(folderName, file)).forEach(file => {
+            if (fs.statSync(file).isDirectory()) {
+                recurse(file)
+            } else {
+                files.push(file)
+            }
+        })
+    }
+
+    recurse(folderName)
+
+    const paths = files.map(name => ({
+        params: {
+            slug: name.replace(".md", "").split("\\").slice(1)
+        }
+    }))
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export default Post
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,40 @@
 import Head from 'next/head'
 import fs from "fs"
 import Link from "next/link"
-import { Header, Footer } from "../util/shared"
-//var Header = require("../util/utils")
+import { Layout } from "../components/shared"
+import { Jumbotron } from 'react-bootstrap'
+import path from "path"
 
-const Home = ({ slugs }) => (<>
-    <Head>
-        <title>Lorenzo Bartolini</title>
-        <link rel="icon" href="/favicon.ico" />
-    </Head>
-    {Header()}
-    <main>
-        <h2>Benvenuto sul mio blog!</h2>
-        <p>Lista delle pagine:</p>
-        {
-            slugs.map(slug => (
-                <div key={slug}>
-                    <Link href={slug}>
-                        <a>{slug}</a>
-                    </Link>
-                </div>
-            ))
-        }
-    </main>
-    {Footer()}
-</>)
+
+const Home = ({ slugs }) => (
+    <Layout>
+        <Head>
+            <title>Lorenzo Bartolini</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Jumbotron>
+            <h2>Benvenuto sul mio blog!</h2>
+            <p>Lista delle pagine:</p>
+            {
+                slugs.map(slug => (
+                    <div key={slug}>
+                        <Link href={slug.path}>
+                            <a>{slug.name}</a>
+                        </Link>
+                    </div>
+                ))
+            }
+        </Jumbotron>
+    </Layout>)
 
 export const getStaticProps = async () => {
-    const files = fs.readdirSync("posts")
+    const files = fs.readdirSync(path.join("pages-md","posts"))
     return {
         props: {
-            slugs: files.map(name => name.replace(".md", ""))
+            slugs: files.map(name => name.replace(".md", "")).map(el => ({
+                "name": el,
+                "path": "posts/" + el
+            }))
         }
     }
 }
