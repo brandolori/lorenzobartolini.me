e2c0c896d258212167b810eeb22decbc9d933da8
lorib <lori.barto@live.it>
2021-12-06 20:46:17 +0100

    rss feed work

---
+++ src/postbuild.js
@@ -0,0 +1,52 @@
+const { Feed } = require("feed")
+const fs = require("fs")
+const matter = require("gray-matter")
+const path = require("path")
+
+const baseUrl = "https://lorenzobartolini.me"
+
+const lorib = {
+    name: "Lorenzo Bartolini",
+    email: "lori.barto@live.it",
+    link: baseUrl
+}
+
+const feed = new Feed({
+    title: "lorenzobartolini.me",
+    description: "Lorenzo Bartolini personal blog",
+    id: baseUrl,
+    link: baseUrl,
+    language: "en",
+    favicon: `${baseUrl}/favicon.ico`,
+    copyright: "2021 Lorenzo Bartolini",
+    author: lorib
+});
+
+const folderPath = path.join("pages-md", "posts")
+const posts = fs.readdirSync(folderPath)
+    .map(filePath => {
+        const string = fs.readFileSync(path.join(folderPath, filePath)).toString()
+        const { content, data } = matter(string)
+        return ({
+            data,
+            url: filePath.replace(".md", "")
+        });
+    })
+posts.forEach(({ data, url }) => {
+    feed.addItem({
+        title: data.title,
+        link: `https://lorenzobartolini.me/posts/${url}`,
+        description: data.summary,
+        author: [lorib]
+    });
+});
+
+feed.addCategory("Tech");
+
+fs.writeFile(path.join('public', 'rss.xml'), feed.rss2(), (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('RSS feed written successfully');
+    }
+});
