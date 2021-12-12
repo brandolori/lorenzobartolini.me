dbf4c29250d0e844b5135c3fd2bb81958cb86b6e
lorib <lori.barto@live.it>
2021-12-13 00:52:04 +0100

    remove scss file, integrate all in _app remove unneded packages update next disable h1 animation on mobile

--- components/AnimatedHeader.tsx
+++ components/AnimatedHeader.tsx
@@ -1,16 +1,18 @@
 import { useContext } from "react"
 import { animated, useSpring } from "react-spring"
 import PointerContext from "../src/PointerContext"
+import useIsMobile from "../src/useIsMobile"
 
 //compute the function y=x/(x+1), mirrored on the y axis.
 const absHyperbole = (x: number) => Math.sign(x) * Math.abs(x) / (Math.abs(x) + 1)
 
 export default ({ title }: { title: string }) => {
     const pointer = useContext(PointerContext)
-    const props = useSpring({
+    const h1Spring = useSpring({
         bottom: absHyperbole(pointer.y) * 10,
         left: absHyperbole(pointer.x) * 10
     })
+    const animatedProps = useIsMobile() ? {} : h1Spring
     return <div>
         <style jsx>{`
         // calibrato circa per smettere di stare nello schermo quando la navbar si accavalla
@@ -31,7 +33,7 @@ export default ({ title }: { title: string }) => {
                     position: "relative",
                     width: "fit-content",
                     margin: "4rem 0",
-                    ...props
+                    ...animatedProps
                 }}>
                 {title}
             </animated.span>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,31 +3,91 @@ import React, { useState } from "react"
 import Layout from "../components/Layout"
 import { GoogleFonts } from "nextjs-google-fonts/GoogleFonts";
 import { theme } from "../src/common"
-import "../styles/global.scss"
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => <>
+    <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/192.png" />
+        <meta name="theme-color" content={theme.mainColor} />
+        {GoogleFonts()}
+    </Head>
 
+    <Layout>
+        <style jsx global>{`
+            :root {
+                --maincolor: ${theme.mainColor} !important;
+                --backgroundcolor: ${theme.backgroundColor} !important;
+            }
 
-    return <>
-        <Head>
-            <link rel="manifest" href="/manifest.json" />
-            <link rel="apple-touch-icon" href="/icons/192.png" />
-            <meta name="theme-color" content={theme.mainColor} />
-            {GoogleFonts()}
-            <style>
-                {`
-                    :root {
-                        --maincolor: ${theme.mainColor} !important;
-                        --backgroundcolor: ${theme.backgroundColor} !important;
-                    }
-                `}
-            </style>
-        </Head>
+            #__next {
+                position: absolute;
+                top: 0px;
+                bottom: 0px;
+                left: 0px;
+                right: 0px;
+            }
+            
+            a {
+                text-decoration-color: var(--maincolor);
+            }
+            
+            html {
+                font-size: 1.25rem;
+                background-color: var(--backgroundcolor);
+            }
+            
+            * {
+                color: white;
+                font-family: Georgia, serif;
+            }
+            
+            h1,
+            h2,
+            h3,
+            h4 {
+                font-family: 'Rubik', sans-serif;
+                color: var(--maincolor);
+            }
+            
+            *::selection {
+                color: #1A1A1A;
+                background: var(--maincolor);
+                text-shadow: none;
+            }
+            
+            p {
+                line-height: 1.5rem;
+            }
+            
+            h1,
+            h2,
+            h3,
+            h4 {
+                text-shadow:
+                    0px 0px 8px var(--backgroundcolor),
+            }
+            
+            p,
+            ol * {
+                text-shadow:
+                    0px 0px 8px var(--backgroundcolor),
+                    0px 0px 8px var(--backgroundcolor);
+            }
+            
+            pre {
+                overflow-x: scroll;
+                background-color: #1a1a1ace;
+                padding: 2rem;
+                border-radius: 8px;
+            }
+            
+            code {
+                font-family: monospace;
+            }
+        `}</style>
+        <Component {...pageProps} />
+    </Layout>
+</>
 
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    </>
-}
 
 export default MyApp
