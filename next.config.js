199fabc1f924bcda5272aa3db1d4a1f6aceebc0a
lorib <lori.barto@live.it>
2021-12-20 21:03:01 +0100

    ..

--- next.config.js
+++ next.config.js
@@ -9,15 +9,6 @@ module.exports = withGoogleFonts({
     },
     webpack: (config) => {
         config.resolve.alias.three$ = path.resolve('./src/three-exports.js')
-        // Replace React with Preact only in client production build
-        if (process.env.NODE_ENV != "development") {
-            Object.assign(config.resolve.alias, {
-                "react": "preact/compat",
-                "react-dom/test-utils": "preact/test-utils",
-                "react-dom": "preact/compat",     // Must be below test-utils
-                "react/jsx-runtime": "preact/jsx-runtime"
-            });
-        }
         return config
     }
 });
