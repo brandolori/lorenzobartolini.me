df74134b2fefa3226587de0a15706a5e9124d241
Lorenzo Bartolini <lbartolini@dynamicait.com>
2022-10-20 17:44:47 +0200

    avoid importing all of three.js. save 120k

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-    reactStrictMode: true,
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const path = require("path")
+/**
+ * @return {import('next').NextConfig}
+ */
+module.exports = (phase, { defaultConfig }) => {
+
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            reactStrictMode: true,
+        }
+    }
+
+    return {
+        reactStrictMode: true,
+        webpack: (config) => {
+            config.resolve.alias.three$ = path.resolve('./src/three-exports.js')
+            return config
+        }
+    }
 }
---
+++ src/three-exports.js
@@ -0,0 +1,38 @@
+import { REVISION } from 'three/src/constants.js'
+
+export { WebGLRenderer } from 'three/src/renderers/WebGLRenderer.js'
+export { Scene } from 'three/src/scenes/Scene.js'
+export { Mesh } from 'three/src/objects/Mesh.js'
+export { Group } from 'three/src/objects/Group.js'
+export { Texture } from 'three/src/textures/Texture.js'
+
+// Export only the geometries and materials that I need
+// export * from 'three/src/geometries/Geometries.js';
+export * from 'three/src/materials/Material.js'
+export * from 'three/src/materials/MeshBasicMaterial.js'
+export * from 'three/src/geometries/TorusGeometry.js'
+export * from 'three/src/geometries/BoxGeometry.js'
+
+export { PerspectiveCamera } from 'three/src/cameras/PerspectiveCamera.js'
+export { OrthographicCamera } from 'three/src/cameras/OrthographicCamera.js' //GENERATES ERRORS
+export { Camera } from 'three/src/cameras/Camera.js'
+export { Raycaster } from 'three/src/core/Raycaster.js'
+export { Layers } from 'three/src/core/Layers.js'
+export { Clock } from 'three/src/core/Clock.js'
+export { BufferGeometry } from 'three/src/core/BufferGeometry'
+export { Vector3 } from 'three/src/math/Vector3.js'
+export { Vector2 } from 'three/src/math/Vector2.js'
+export { Color } from 'three/src/math/Color.js'
+export { sRGBEncoding, PCFSoftShadowMap, LinearEncoding, NoToneMapping, ACESFilmicToneMapping } from 'three/src/constants.js'
+
+if (typeof __THREE_DEVTOOLS__ !== 'undefined') {
+
+    /* eslint-disable no-undef */
+    __THREE_DEVTOOLS__.dispatchEvent(new CustomEvent('register', {
+        detail: {
+            revision: REVISION,
+        }
+    }))
+    /* eslint-enable no-undef */
+
+}
